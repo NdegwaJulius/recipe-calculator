69ac142cf4b1d508d9cf69e4712c33776696f0bd
NdegwaJulius <juliusndegwa2@gmail.com>
2022-10-25 10:47:20 +0300

    Adding ingredients & displaying them.

--- lib/model/recipe_detail.dart
+++ lib/model/recipe_detail.dart
@@ -34,6 +34,16 @@ class _RecipeDetailState extends State<RecipeDetail> {
               widget.recipe.label,
               style: const TextStyle(fontSize: 18),
             ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: widget.recipe.ingredients.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final ingredient = widget.recipe.ingredients[index];
+                  return Text(
+                      '${ingredient.quantity} ${ingredient.measure} ${ingredient.name}');
+                },
+              ),
+            ),
           ],
         ),
       ),
